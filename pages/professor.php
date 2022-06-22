ac69e44382b34f4a78207d0b97e5b0d72737cc8d
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-06-21 22:19:07 -0300

    Update professor.php

--- pages/professor.php
+++ pages/professor.php
@@ -3,6 +3,19 @@
         <div class="card-body">
             <h6 class="card-title">Professores</h6>
             <table id="professores" class="table table-striped table-bordered">
+              
+            <form action="professor.php" method="POST">
+                <div class="form-group">
+                    <input name="nome" id="nome" type="text" class="form-control" placeholder="Nome do Professor" required autofocus>
+                </div>
+                <div class="form-group">
+                    <input name="disciplina" id="disciplina" type="text" class="form-control" placeholder="Disciplina" required>
+                </div>
+                <div class="form-group">
+                    <input name="periodo" id="periodo" type="text" class="form-control" placeholder="Semestre" required>
+                </div>
+                <button class="btn btn-primary btn-block">Cadastrar</button>
+           </form>
               <thead>
                 <tr>
                   <th>Nome</th>
@@ -27,3 +40,30 @@
         $('#professores').DataTable();
     });    
 </script>
+
+<?php
+
+if (!empty($_POST['nome'])) {
+    include("./includes/conn.php");
+
+    $nome       = $_POST['nome'];
+    $disciplina  = $_POST['disciplina'];
+    $periodo      = $_POST['semestre'];
+   
+
+    //string SQL
+    $sql = "insert into Professores 
+    (nome, disciplina, periodo) 
+    values ('".$nome."', '".$disciplina."', '".$periodo."');";
+
+    //Execução da Consulta
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Inserido com sucesso!')</script>";
+    } else {
+        echo "<script>alert('Falha ao inserir dado!')</script>";
+    }
+
+    //Fechamento da Conexão
+    mysqli_close($conn);
+}
+?>
