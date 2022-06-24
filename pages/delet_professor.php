2ef47089fa1fe1fe8d36bc582a68a67d07b832a8
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-06-24 17:38:49 -0300

    delete_funcionando

--- pages/delet_professor.php
+++ pages/delet_professor.php
@@ -1,14 +1,16 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
 
-    include("./includes/conn.php");
+    include("../includes/conn.php");
     
     $id_professor = $_GET["id"];
-    
-    $sql = "DELETE FROM professores WHERE id = '$id_professor'";
-    var_dump(mysqli_connect($conn,$sql));
+    echo $id_professor;
+  
 
+    $sql = "DELETE FROM professores WHERE id = '$id_professor'";
     echo $sql;
-    
+    var_dump(mysqli_query($conn,$sql));
  
    
     mysqli_close($conn);
