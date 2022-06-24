aea09136353a5061fbe528723a364ad966032d28
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2022-06-24 13:38:30 -0300

    backup_BD

--- crm_academico.sql
+++ crm_academico.sql
@@ -33,15 +33,6 @@ CREATE TABLE `Cst_gti` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Cst_gti`
---
-
-LOCK TABLES `Cst_gti` WRITE;
-/*!40000 ALTER TABLE `Cst_gti` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Cst_gti` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Cst_has_Disciplinas`
 --
 
@@ -60,15 +51,6 @@ CREATE TABLE `Cst_has_Disciplinas` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Cst_has_Disciplinas`
---
-
-LOCK TABLES `Cst_has_Disciplinas` WRITE;
-/*!40000 ALTER TABLE `Cst_has_Disciplinas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Cst_has_Disciplinas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Disciplinas`
 --
 
@@ -82,43 +64,6 @@ CREATE TABLE `Disciplinas` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Disciplinas`
---
-
-LOCK TABLES `Disciplinas` WRITE;
-/*!40000 ALTER TABLE `Disciplinas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Disciplinas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Professores`
---
-
-DROP TABLE IF EXISTS `Professores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Professores` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) DEFAULT NULL,
-  `disciplina` varchar(45) DEFAULT NULL,
-  `periodo` varchar(45) DEFAULT NULL,
-  `Disciplinas_id` int unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_Professores_Disciplinas_idx` (`Disciplinas_id`),
-  CONSTRAINT `fk_Professores_Disciplinas` FOREIGN KEY (`Disciplinas_id`) REFERENCES `Disciplinas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Professores`
---
-
-LOCK TABLES `Professores` WRITE;
-/*!40000 ALTER TABLE `Professores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Professores` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `alunos`
@@ -140,15 +85,6 @@ CREATE TABLE `alunos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `alunos`
---
-
-LOCK TABLES `alunos` WRITE;
-/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `alunos_has_Disciplinas`
 --
 
@@ -167,13 +103,20 @@ CREATE TABLE `alunos_has_Disciplinas` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `alunos_has_Disciplinas`
+-- Table structure for table `professores`
 --
 
-LOCK TABLES `alunos_has_Disciplinas` WRITE;
-/*!40000 ALTER TABLE `alunos_has_Disciplinas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alunos_has_Disciplinas` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `professores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `disciplina` varchar(45) DEFAULT NULL,
+  `periodo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `usuario`
@@ -191,16 +134,6 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (6,'Vinicios Matheus ','oliveira da silva','vinicios471@gmail.com','81dc9bdb52d04dc20036dbd8313ed055'),(7,'VINICIOS MATHEUS OLIVEIRA','SILVA','vinicios471@gmail.com','8c04100d32d5021e53e626b3528b9a2d'),(8,'thiago nogueira','','','d41d8cd98f00b204e9800998ecf8427e'),(9,'Tiago','','','d41d8cd98f00b204e9800998ecf8427e');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -211,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-22 12:47:15
+-- Dump completed on 2022-06-24 13:37:54
